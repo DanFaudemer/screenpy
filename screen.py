81ec1786994fdf28f8fd6fdb2958a009ce3f8a52
dan <dan.faudemer@neuf.fr>
2016-12-21 15:15:31 +0100

    Only use gtk

--- screen.py
+++ screen.py
@@ -1,8 +1,7 @@
 # camera.py
 
-import cv2
-import numpy
-import gtk, Image
+
+import gtk
 import time
 
 
@@ -11,8 +10,14 @@ class VideoCamera:
         self.img_width = gtk.gdk.screen_width()
         self.img_height = gtk.gdk.screen_height()
         self.img_quality = img_quality
+        self.img_buffer = ""
+    def gtk_cb(self, buf):
+        self.img_buffer += buf
+        #print(buf)
+
 
     def get_frame(self):
+        self.img_buffer = ""
         try:
             screengrab = gtk.gdk.Pixbuf(
             gtk.gdk.COLORSPACE_RGB,
@@ -30,32 +35,17 @@ class VideoCamera:
             self.img_height
             )
 
+
+            #print ("Done")
         except:
             print "Failed taking screenshot"
-
-        #print "Converting to PIL image ..."
-        final_screengrab = Image.frombuffer(
-        "RGB",
-        (self.img_width, self.img_height),
-        screengrab.get_pixels(),
-        "raw",
-        "RGB",
-        screengrab.get_rowstride(),
-        1
-        )
-
-        image = numpy.array(final_screengrab.convert('RGB'))
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, self.img_quality])
-        return jpeg.tobytes()
-
+        screengrab.save_to_callback(self.gtk_cb, 'jpeg',  {"quality":str(self.img_quality)})
+        return self.img_buffer
 
 #Use for speed test
 if __name__ == '__main__':
     n_frames = 100
-    cam = VideoCamera(5)
+    cam = VideoCamera(100)
     start = time.time()
     for i in range(0,n_frames):
         cam.get_frame()
